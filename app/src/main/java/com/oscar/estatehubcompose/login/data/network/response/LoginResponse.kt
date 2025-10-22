97f7ee81e1bd5f62573d36c7014364141a8c5588
oscar <oscarmancilla227@gmail.com>
2025-10-22 13:05:25 -0600

    add: setters LoginViewModel agregados

--- app/src/main/java/com/oscar/estatehubcompose/login/data/network/response/LoginResponse.kt
+++ app/src/main/java/com/oscar/estatehubcompose/login/data/network/response/LoginResponse.kt
@@ -2,6 +2,7 @@ package com.oscar.estatehubcompose.login.data.network.response
 
 import com.google.gson.annotations.SerializedName
 
-data class LoginResponse(@SerializedName("sucess") val success: Boolean,
+data class LoginResponse(@SerializedName("message") val message: String,
+                         @SerializedName("sucess") val success: Boolean,
                          @SerializedName("token") val token: String) {
 }
--- app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginViewModel.kt
@@ -1,5 +1,7 @@
 package com.oscar.estatehubcompose.login.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.oscar.estatehubcompose.login.data.network.request.LoginRequest
@@ -10,6 +12,23 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase): ViewModel() {
+
+    private var _correo = MutableLiveData<String>();
+    private var _contrasenia = MutableLiveData<String>();
+
+    var correo: LiveData<String> = _correo;
+    var contrasenia: LiveData<String> = _contrasenia;
+
+    fun setCorreo(correo: String){
+        _correo.value = correo;
+    }
+
+    fun setContrasenia(contrasenia: String){
+        _contrasenia.value = contrasenia;
+    }
+
+
+
 
     fun onLogin(user: String, password: String){
         val loginRequest: LoginRequest = LoginRequest(user, password);
