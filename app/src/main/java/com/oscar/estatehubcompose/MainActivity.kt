183c4c663622920b05a5b23b1bb99f89ed3bec9c
oscar <oscarmancilla227@gmail.com>
2025-10-29 12:01:29 -0600

    add: test login screen, personalizedTheme

--- app/src/main/java/com/oscar/estatehubcompose/MainActivity.kt
+++ app/src/main/java/com/oscar/estatehubcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.oscar.estatehubcompose.login.ui.LoginScreen
 import com.oscar.estatehubcompose.login.ui.LoginViewModel
 import com.oscar.estatehubcompose.ui.theme.EstateHubComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,7 +30,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             EstateHubComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(text = "Hello World", modifier = Modifier.padding(innerPadding));
+
+                    Column(Modifier.padding(innerPadding)) {
+                        LoginScreen();
+                    }
                 }
             }
         }
--- app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginScreen.kt
+++ app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginScreen.kt
@@ -1,14 +1,32 @@
 package com.oscar.estatehubcompose.login.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.oscar.estatehubcompose.ui.theme.PrimaryPersonalized
+import com.oscar.estatehubcompose.ui.theme.SecondaryPersonalized
 
 @Composable
 fun LoginScreen(){
 
     Column {
-
+        PreviewLoginScreen();
     }
 
 }
@@ -16,5 +34,36 @@ fun LoginScreen(){
 @Composable
 @Preview
 fun PreviewLoginScreen(){
+    Column(Modifier.fillMaxSize().padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        Text("Iniciar sesion");
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            shape = RoundedCornerShape(10.dp),
+            placeholder = {
+            Text("Correo electronico")
+        })
+
+        Spacer(Modifier.padding(5.dp));
+        OutlinedTextField(value = "",
+            onValueChange = {},
+            shape = RoundedCornerShape(10.dp),
+            placeholder = {
+            Text("Contraseña")
+        })
+
+
+        Button(onClick = {},
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.secondary
+            ),
+            modifier = Modifier.padding(5.dp)) {
+            Text("Iniciar sesion");
+        }
+    }
 
 }
