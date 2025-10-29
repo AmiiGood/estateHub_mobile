82df8444acab95155fffb88f41a016ba9fc0c700
oscar <oscarmancilla227@gmail.com>
2025-10-29 14:01:44 -0600

    add: componentes pantalla login

--- app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginScreen.kt
+++ app/src/main/java/com/oscar/estatehubcompose/login/ui/LoginScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -17,8 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.oscar.estatehubcompose.ui.theme.PrimaryPersonalized
 import com.oscar.estatehubcompose.ui.theme.SecondaryPersonalized
 
@@ -34,14 +37,20 @@ fun LoginScreen(){
 @Composable
 @Preview
 fun PreviewLoginScreen(){
-    Column(Modifier.fillMaxSize().padding(10.dp),
+    Column(Modifier.fillMaxSize().padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
 
-        Text("Iniciar sesion");
+        Column(horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxWidth()){
+
+            Text("Iniciar sesion", style = TextStyle(fontSize = 40.sp));
+            Text("Accede a tu cuenta para continuar", style = TextStyle(fontSize = 16.sp));
+
+        }
         OutlinedTextField(value = "",
             onValueChange = {},
             shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth(),
             placeholder = {
             Text("Correo electronico")
         })
@@ -50,10 +59,12 @@ fun PreviewLoginScreen(){
         OutlinedTextField(value = "",
             onValueChange = {},
             shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth(),
             placeholder = {
             Text("Contraseña")
         })
 
+        Text("Olvidaste tu contraseña?")
 
         Button(onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -61,9 +72,20 @@ fun PreviewLoginScreen(){
                 containerColor = MaterialTheme.colorScheme.primary,
                 contentColor = MaterialTheme.colorScheme.secondary
             ),
-            modifier = Modifier.padding(5.dp)) {
+            modifier = Modifier.padding(5.dp).fillMaxWidth()) {
             Text("Iniciar sesion");
+
+
         }
+
+        Button(onClick = {},
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.padding(5.dp).fillMaxWidth()) {
+            Text("Registrarse")
+        }
+
+
+
     }
 
 }
