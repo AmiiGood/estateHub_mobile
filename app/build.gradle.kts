e8e7ddf72f97a7593aa77f5ac71747f48ba8436b
oscar <oscarmancilla227@gmail.com>
2025-10-31 14:39:57 -0600

    update: analisisScreen mapa y ubicacion automatica agregados

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.maps.android:maps-compose:4.4.1")
     implementation("com.google.accompanist:accompanist-permissions:0.34.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
--- app/src/main/java/com/oscar/estatehubcompose/analisis/ui/AnalisisScreen.kt
+++ app/src/main/java/com/oscar/estatehubcompose/analisis/ui/AnalisisScreen.kt
@@ -1,29 +1,73 @@
 package com.oscar.estatehubcompose.analisis.ui
 
+import android.annotation.SuppressLint
+import android.location.Location
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.rememberCameraPositionState
 
-
+@SuppressLint("MissingPermission")
 @Composable
 fun AnalisisScreen(modifier: Modifier){
+    val context = LocalContext.current;
+    var ubicacionDispositivo by remember { mutableStateOf<Location?>(null) };
+    val fusedLocationClient = remember {
+        LocationServices.getFusedLocationProviderClient(context);
+    };
+    var coordenadaInicial = LatLng(21.0190,-101.2574 );
+    val cameraPosition = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(coordenadaInicial, 25f)
+    }
 
-    Mapa(modifier);
+
+    LaunchedEffect(Unit) {
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                ubicacionDispositivo = it;
+                Log.d("Ubicacion", "Ubicación obtenida: ${it.latitude}, ${it.longitude}")
+                val nuevaUbicacion = LatLng(it.latitude, it.longitude)
+                cameraPosition.position = CameraPosition.fromLatLngZoom(nuevaUbicacion, 17f)
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("Ubicacion", "Error obteniendo ubicación: ${exception.message}")
+        }
+
+
+
+        coordenadaInicial = LatLng(ubicacionDispositivo?.latitude ?: -101.2574,
+            ubicacionDispositivo?.longitude ?: 21.0190)
+    }
+
+
+
+    Mapa(modifier, cameraPosition);
 
 }
 
 
 @Composable
-fun Mapa(modifier: Modifier){
+fun Mapa(modifier: Modifier, cameraPosition: CameraPositionState){
 
     val mapaType by remember{ mutableStateOf(MapProperties(mapType = MapType.TERRAIN)) }
 
-    GoogleMap(modifier = Modifier.fillMaxSize(), properties = mapaType) {  }
+    GoogleMap(modifier = Modifier.fillMaxSize(),
+        properties = mapaType, cameraPositionState =  cameraPosition) {  }
 }
